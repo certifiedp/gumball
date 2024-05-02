ef4118bc8d9378e17bb97ed8b6cc52e07637e49a
Marc <mkarim117@berkeley.edu>
2024-05-01 21:08:34 -0700

    connect button + background page

---
+++ gumball/pages/index.tsx
@@ -0,0 +1,22 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import type { NextPage } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Gumball Application</title>
+      </Head>
+
+      <main className={styles.main}>
+        <ConnectButton />
+
+        <h1> Connect your Wallet </h1>
+      </main>
+    </div>
+  );
+};
+
+export default Home;
